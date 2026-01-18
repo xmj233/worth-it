e8505040891fd19a403af0a40f2fb25223dc36da
andrew <543055255@qq.com>
2026-01-18 22:43:08 +0800

    add tauri

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    hello world.
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   ssr: false,
-})
+  devServer: { host: "0.0.0.0" },
+  nitro: { preset: "static" },
+});
