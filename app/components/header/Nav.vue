86bd7338bb2af55d63d4f80bbc2015eaa516571a
andrew <543055255@qq.com>
2026-01-28 23:04:38 +0800

    icon design

--- app/components/header/Nav.vue
+++ app/components/header/Nav.vue
@@ -4,46 +4,46 @@ import type { NavigationMenuItem } from '@nuxt/ui'
 const items = ref<NavigationMenuItem[]>([
   {
     label: 'Where It Went',
-    icon: 'i-lucide-book-open',
+    icon: 'i-lucide-milestone',
     to: '',
     children: [
       {
         label: 'Now',
         description: 'What’s happening lately.',
-        icon: 'i-lucide-house'
+        icon: 'i-lucide-map-pin'
       },
       {
         label: 'Then',
         description: 'What happened before.',
-        icon: 'i-lucide-cloud-download'
+        icon: 'i-lucide-footprints'
       },
     ]
   },
   {
-    label: 'Inside',
-    icon: 'i-lucide-book-open',
+    label: 'Context',
+    icon: 'i-lucide-layers-2',
     to: '',
     children: [
       {
         label: 'Labels',
         description: 'How you name what matters.',
-        icon: 'i-lucide-house'
+        icon: 'i-lucide-tag'
       },
       {
         label: 'Mood',
         description: 'Patterns of how you feel.',
-        icon: 'i-lucide-cloud-download'
+        icon: 'i-lucide-smile'
       },
       {
         label: 'Wishes',
         description: 'What you’re quietly hoping for.',
-        icon: 'i-lucide-cloud-download'
+        icon: 'i-lucide-sparkles'
       },
     ]
   },
   {
     label: 'Settings',
-    icon: 'i-lucide-book-open',
+    icon: 'i-lucide-bolt',
     to: '',
   }
 ])
